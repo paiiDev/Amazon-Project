93374a95dd6abef3718991ebec888bec495b9985
paiiDev <pyaepaikyaw@gmail.com>
2024-11-06 12:48:02 +0630

    Make add to cart button iteractive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,9 +1,9 @@
 console.log("Hello");
 
-let productsHTML = '';
+let productsHTML = "";
 
 products.forEach((product) => {
-  productsHTML  += `<div class="product-container">
+  productsHTML += `<div class="product-container">
           <div class="product-image-container">
             <img class="product-image" src="${product.image}">
           </div>
@@ -13,14 +13,16 @@ products.forEach((product) => {
           </div>
 
           <div class="product-rating-container">
-            <img class="product-rating-stars" src="images/ratings/rating-${product.rating.stars * 10}.png">
+            <img class="product-rating-stars" src="images/ratings/rating-${
+              product.rating.stars * 10
+            }.png">
             <div class="product-rating-count link-primary">
               ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-           $ ${(product.priceCents/100).toFixed(2)}
+           $ ${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -45,13 +47,37 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            product.id
+          }" >
             Add to Cart
           </button>
         </div>`;
-
 });
 
-console.log(productsHTML);
-
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
+
+document.querySelectorAll(".js-add-to-cart").forEach((button) => {
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    })
+
+    if(matchingItem){
+      matchingItem.quatity +=1;
+    } else {
+         cart.push({
+           productId: productId,
+           quatity: 1,
+         });
+    }
+ 
+    console.log(cart);
+  });
+});
